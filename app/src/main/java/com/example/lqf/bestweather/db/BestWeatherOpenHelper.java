82d729c240045753e71e05744054580193204d2b
TanlentFeng <604648286@qq.com>
2015-09-20 20:43:04 +0800

    add the dbhelper and entity of the db

---
+++ app/src/main/java/com/example/lqf/bestweather/db/BestWeatherOpenHelper.java
@@ -0,0 +1,42 @@
+package com.example.lqf.bestweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by LQF on 2015/9/20.
+ */
+public class BestWeatherOpenHelper extends SQLiteOpenHelper {
+    public static final String CREATE_PROVINCE="create table Province(" +
+            "id integer primary key autoincrement," +
+            "province_name text," +
+            "province_code text" +
+            ")";
+    public static final String CREATE_CITY="create table City(" +
+            "id integer primary key autoincrement," +
+            "city_name text," +
+            "city_code text" +
+            "province_code integer" +
+            ")";
+    public static final String  CREATE_COUNTY="create table County(" +
+            "id integer primary key auto increment," +
+            "county_name text," +
+            "county_code text" +
+            "city_code integer" +
+            ")";
+    public BestWeatherOpenHelper(Context context,String name,SQLiteDatabase.CursorFactory factory,int version){
+
+        super(context,name,factory,version);
+    }
+    public void onCreate(SQLiteDatabase database){
+        database.execSQL(CREATE_PROVINCE);
+        database.execSQL(CREATE_CITY);
+        database.execSQL(CREATE_COUNTY);
+
+
+    }
+    public void onUpgrade(SQLiteDatabase database,int i,int version){
+
+    }
+}
